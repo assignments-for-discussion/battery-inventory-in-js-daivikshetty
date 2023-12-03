ea7562d89a7d695f8f5c8e0fc20e408b5e6e2aa8
Daivik Shetty <84132439+daivikshetty@users.noreply.github.com>
2023-12-03 17:24:39 +0530

    Update main.js
    updated countBatteriesByHealth function

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 const assert = require('assert');
 
 function countBatteriesByHealth(presentCapacities) {
-  return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
+  var countHealthOfBatteries = {          //stores the count of each classification of battery
+      healthy: 0,
+      exchange: 0,
+      failed: 0
   };
+
+  for(var i=0;i<presentCapacities.length;i++){
+
+      var stateOfHealth = (presentCapacities[i]/120)*100;  //calculate state-of-healt of battery
+
+      if(stateOfHealth >= 81){      //check if battery is 'healthy'
+            countHealthOfBatteries.healthy += 1;
+      }
+      else if(stateOfHealth <= 80 && stateOfHealth >= 62){  //check if battery is 'exchange'
+            countHealthOfBatteries.exchange += 1;
+      }
+      else{       //battery is assigned 'failed'
+            countHealthOfBatteries.failed += 1;
+      }
+
+  }
+  return countHealthOfBatteries;
 }
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
+
   const presentCapacities = [113, 116, 80, 95, 92, 70];
   counts = countBatteriesByHealth(presentCapacities);
+
   assert(counts["healthy"] == 2);
   assert(counts["exchange"] == 3);
   assert(counts["failed"] == 1);
+
   console.log("Done counting :)");
 }
 
